c22e921d9e15754d6cf1cbbe3afed257e0b4b275
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-05 14:40:12 -0400

    Update count-vowels.js

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const isVowel = (char) => {
+  let vowels = "aeuioAEUIO";
+  return vowels.indexOf(char) > -1 ? 1 : 0;
+}
+
+const countVowels = (str) => {
+  let count = 0;
+  
+  if (!(str.length)) return 0;
+  
+  if (isVowel(str[0])) count += 1;
+  return count +  countVowels(str.slice(1));
+}

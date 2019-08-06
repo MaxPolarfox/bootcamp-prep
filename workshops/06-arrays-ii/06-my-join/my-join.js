88e229c5cf22bba853f5791e6d64a41d2cc9204c
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-06 08:36:23 -0400

    Update my-join.js

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+const myJoin = (arr, sep) => {
+  let str = '';
+  
+  if (sep === undefined) sep = ',';
+  
+  for (let val of arr) {
+    if (!val) str += sep;
+      else str += val +sep;
+   }
+  return str.slice(0, str.length - sep.length)
+}

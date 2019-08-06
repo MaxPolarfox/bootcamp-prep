ea3a813bc0574520df54653baf07c0d84f7bf684
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-06 08:40:29 -0400

    Update my-slice.js

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const mySlice = (arr, start = 0, end = arr.length) => {
+  let copy = [];
+  
+  if (start < 0) start = arr.length + start
+  if (end < 0) end = arr.length + end;
+  
+  for (let i = start; i < end; i++) copy.push(arr[i]);
+  
+  return copy
+}

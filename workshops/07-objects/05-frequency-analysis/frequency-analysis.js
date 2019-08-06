2c7d4b22e6537567507a451350c8aaa9d37954e4
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-06 14:20:34 -0400

    Update frequency-analysis.js

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const frequencyAnalysis = (str) => {
+  let obj = {};
+  
+  for (let char of str) {
+    if (obj[char]) obj[char]++;
+    else obj[char] = 1;
+  }
+  return obj
+}

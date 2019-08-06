533130e59c163712f108e23b486f0d761d0335f7
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-06 10:38:03 -0400

    Update leet-translator.js

--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -2,3 +2,12 @@ let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
 
 // YOUR CODE BELOW
+const leetTranslator = (str) => {
+  str = str.toLowerCase();
+  let translated = '';
+  
+  for (let char of str) {
+    translated += leetChars[letters.indexOf(char)];
+  }
+  return translated
+}

2edb24188cec31a124f262d87e625acafe780cc8
Maksim Pesetski <52350745+MaxPolarfox@users.noreply.github.com>
2019-08-06 09:59:09 -0400

    Update rotate-array.js

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+const rotateArray = (arr, num) => {
+   
+  let copy = arr.slice();
+  
+  if (num > 0) {
+    while (num > 0) {
+      copy.unshift(copy.pop());
+      num --;
+    }
+  } else {
+    while (num < 0) {
+      copy.push(copy.shift());
+      num++;
+    }
+  }
+  return copy
+}
